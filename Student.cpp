6413b61081426db36a2384d37a3341f77c5dbb98
Ertusta <ertugrulsahinturk6161@gmail.com>
2024-10-22 20:58:17 +0300

    update0

---
+++ Student.cpp
@@ -0,0 +1,14 @@
+#include <string>
+
+using namespace std;
+
+class Student {
+public:
+    string ad[100];         
+    string ogrNo[100];       
+    float araSinav[100];    
+    float ikinciSinav[100];  
+    float odev[100];        
+    float finalNotu[100];    
+    int devamSayisi[100];    
+};
