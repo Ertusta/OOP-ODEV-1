5add7b712efdadbee805f34523c7ac89c2efbc9d
Ertusta <ertugrulsahinturk6161@gmail.com>
2024-10-23 22:53:51 +0300

    constructer and destructer

--- Ertusta.cpp
+++ Ertusta.cpp
@@ -1,9 +1,58 @@
+#include <string>
 
-#include <iostream>
 using namespace std;
 
-int main() {
-    cout << "Hello, World!";
-    return 0;
+class Student
+{
 
+    string *ad;
+    string *ogrNo;
+    float *araSinav;
+    float *ikinciSinav;
+    float *odev;
+    float *final;
+    int *devamSayisi;
+
+    // constructer
+    Student(int size)
+    {
+
+        ad = new string[size];
+        ogrNo = new string[size];
+        araSinav = new float[size];
+        ikinciSinav = new float[size];
+        odev = new float[size];
+        final = new float[size];
+        devamSayisi = new int[size];
+    }
+
+    // destrcuter
+    ~Student()
+    {
+        delete[] ad;
+        delete[] ogrNo;
+        delete[] araSinav;
+        delete[] ikinciSinav;
+        delete[] odev;
+        delete[] final;
+        delete[] devamSayisi;
+    }
+
+    void readFromCSV()
+    {
+    }
+
+    void average()
+    {
+    }
+
+    void print()
+    {
+    }
+};
+
+int main()
+{
+
+    return 96;
 }
--- Student.cpp
+++ Student.cpp
@@ -2,13 +2,57 @@
 
 using namespace std;
 
-class Student {
-public:
-    string ad[100];         
-    string ogrNo[100];       
-    float araSinav[100];    
-    float ikinciSinav[100];  
-    float odev[100];        
-    float finalNotu[100];    
-    int devamSayisi[100];    
+class Student
+{
+
+    string *ad;
+    string *ogrNo;
+    float *araSinav;
+    float *ikinciSinav;
+    float *odev;
+    float *final;
+    int *devamSayisi;
+
+    // constructer
+    Student(int size)
+    {
+
+        ad = new string[size];
+        ogrNo = new string[size];
+        araSinav = new float[size];
+        ikinciSinav = new float[size];
+        odev = new float[size];
+        final = new float[size];
+        devamSayisi = new int[size];
+    }
+
+    // destrcuter
+    ~Student()
+    {
+        delete[] ad;
+        delete[] ogrNo;
+        delete[] araSinav;
+        delete[] ikinciSinav;
+        delete[] odev;
+        delete[] final;
+        delete[] devamSayisi;
+    }
+
+    void readFromCSV()
+    {
+    }
+
+    void average()
+    {
+    }
+
+    void print()
+    {
+    }
 };
+
+int main()
+{
+
+    return 96;
+}
